030c05a297200dd12540546c9eb2e87883e7c419
missstaff <shawnastaff@gmail.com>
2023-02-20 18:32:10 -0800

    minor change to show all three ways the naviagtion object can be accessed

--- src/Screens/ContactScreen.js
+++ src/Screens/ContactScreen.js
@@ -1,25 +1,23 @@
 import React from "react";
 import { Text, TouchableOpacity, View } from "react-native";
-import { useNavigation } from "@react-navigation/native";
 
 import { styles } from "../CSS/styles";
 
-const ContactScreen = () => {
+const ContactScreen = (props) => {
 
-  const navigation = useNavigation();
   
   return (
     <View>
       <Text style={styles.h1}>ContactScreen</Text>
       <View style={styles.containerBtn}>
         <TouchableOpacity 
-          onPress={() => [navigation.navigate("Home")]}
+          onPress={() => [props.navigation.navigate("Home")]}
           style={styles.btn}
         >
           <Text style={styles.textBtn}>Home</Text>
         </TouchableOpacity>
         <TouchableOpacity 
-          onPress={() => [navigation.navigate("Cart")]}
+          onPress={() => [props.navigation.navigate("Cart")]}
           style={styles.btn}
           >
           <Text style={styles.textBtn}>Cart</Text>
